38d74a16654fef2d644112ee6c0617356c853359
levchenko1986 <vadimleva1986@gmail.com>
2021-12-09 14:26:01 +0200

    Removed redme and jpg

--- src/components/transaction-history/transactions.jsx
+++ src/components/transaction-history/transactions.jsx
@@ -25,10 +25,13 @@ const TransactionHistory = ({ items }) => (
 );
 
 TransactionHistory.propTypes = {
-  id: PropTypes.string,
-  type: PropTypes.string,
-  amount: PropTypes.number,
-  currency: PropTypes.string,
-};
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      type: PropTypes.string,
+      amount: PropTypes.string,
+      currency: PropTypes.string,
+}).isRequired)
+}
 
 export default TransactionHistory;
