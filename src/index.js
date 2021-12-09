f1945ee675bb7465bef6a12535aacf9982ce789c
levchenko1986 <vadimleva1986@gmail.com>
2021-12-09 13:38:13 +0200

    fix

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+console.log(App);
+console.log(document.body);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
